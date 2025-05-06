12e2705d25d314843cf91e50904d734df5de5b40
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-05-06 10:38:34 +0200

    add at-trixi_test_nowarn

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using TrixiTest
 
 @testset verbose=true "TrixiTest.jl Tests" begin
     include("test_aqua.jl")
+    include("test_trixi_test_nowarn.jl")
 end
---
+++ test/test_trixi_test_nowarn.jl
@@ -0,0 +1,22 @@
+macro my_test_nowarn_string(expr, additional_ignore_content = [])
+    quote
+        add_to_additional_ignore_content = ["[ Info: hi"]
+        append!($additional_ignore_content, add_to_additional_ignore_content)
+        @trixi_test_nowarn $expr $additional_ignore_content
+    end
+end
+
+macro my_test_nowarn_regex(expr, additional_ignore_content = [])
+    quote
+        add_to_additional_ignore_content = [r".*hi"]
+        append!($additional_ignore_content, add_to_additional_ignore_content)
+        @trixi_test_nowarn $expr $additional_ignore_content
+    end
+end
+
+@testset "@trixi_test_nowarn" begin
+    @trixi_test_nowarn 1 + 1
+
+    @my_test_nowarn_string @info "hi"
+    @my_test_nowarn_regex @info "hi"
+end
