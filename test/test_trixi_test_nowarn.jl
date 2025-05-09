d73f2b5237ba9507382b42d0cf78940363f0aff7
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-05-09 16:51:17 +0200

    escape expression in tests

--- test/test_trixi_test_nowarn.jl
+++ test/test_trixi_test_nowarn.jl
@@ -2,7 +2,7 @@ macro my_test_nowarn_string(expr, additional_ignore_content = [])
     quote
         add_to_additional_ignore_content = ["[ Info: hi"]
         append!($additional_ignore_content, add_to_additional_ignore_content)
-        @trixi_test_nowarn $expr $additional_ignore_content
+        @trixi_test_nowarn $(esc(expr)) $additional_ignore_content
     end
 end
 
@@ -10,7 +10,7 @@ macro my_test_nowarn_regex(expr, additional_ignore_content = [])
     quote
         add_to_additional_ignore_content = [r".*hi"]
         append!($additional_ignore_content, add_to_additional_ignore_content)
-        @trixi_test_nowarn $expr $additional_ignore_content
+        @trixi_test_nowarn $(esc(expr)) $additional_ignore_content
     end
 end
 
@@ -19,4 +19,6 @@ end
 
     @my_test_nowarn_string @info "hi"
     @my_test_nowarn_regex @info "hi"
+    a = 1.0
+    @my_test_nowarn_string println(a)
 end
