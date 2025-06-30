0fbbe5f063d4b5ae8e1604b759613a74d16b6dd9
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-06-30 15:43:14 +0200

    add some more tests

--- test/test_test_trixi_include.jl
+++ test/test_test_trixi_include.jl
@@ -16,10 +16,15 @@
             @trixi_test_nowarn trixi_include(@__MODULE__, path, x = 7)
 
             @test x == 7
+
+            @test_trixi_include(path, x = 9)
+
+            @test @isdefined x
+            @test x == 9
         end
     end
 
-    @trixi_testset "with l2" begin
+    @trixi_testset "with l2 and linf" begin
         example = """
             function analysis_callback(sol)
              return sol[1], sol[2]
@@ -31,7 +36,7 @@
             write(io, example)
             close(io)
 
-            @test_trixi_include(path, l2=1.0)
+            @test_trixi_include(path, l2=1.0, linf=2.0)
         end
     end
 end
