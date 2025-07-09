a50b759327705b87403bf9760a9ce92deb23b38b
Benedict Geihe <bgeihe@uni-koeln.de>
2025-07-09 12:18:24 +0200

    re-export trixi_include

--- src/TrixiTest.jl
+++ src/TrixiTest.jl
@@ -6,7 +6,10 @@ using TrixiBase: mpi_isroot, trixi_include
 include("auxiliary.jl")
 include("macros.jl")
 
-export get_kwarg, mpi_isroot
+export get_kwarg
 export @trixi_test_nowarn, @test_trixi_include_base, @timed_testset, @trixi_testset
+
+# re-export methods from TrixiBase
+export mpi_isroot, trixi_include
 
 end # module TrixiTest
--- test/test_testset.jl
+++ test/test_testset.jl
@@ -1,5 +1,6 @@
 @timed_testset "@timed_testset" begin
     sleep(0.2)
+    @test true
 end
 
 module TestTrixiTest
